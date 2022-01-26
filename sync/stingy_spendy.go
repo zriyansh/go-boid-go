c45bd9f9407fd3d9c3e2336987177f80a54f59a0
zriyansh <prik2two@gmail.com>
2022-01-26 19:05:36 +0530

    race-condition

---
+++ sync/stingy_spendy.go
@@ -0,0 +1,37 @@
+// to represent race condition, when stingy adds 10 and spendy substracts 10
+// at the same time
+
+package main
+
+import (
+	"time"
+)
+
+var money = 100
+
+func stingy() {
+	for i := 1; i <= 1000; i++ {
+		money += 10
+		time.Sleep(1 * time.Millisecond)
+	}
+	println("stingy Done")
+}
+
+func spendy() {
+	for i := 1; i <= 1000; i++ {
+		money -= 10
+		time.Sleep(1 * time.Millisecond)
+	}
+	println("spendy Done")
+}
+
+func main() {
+	// both are initialised as threads, as 'go' is mentioned
+	go stingy()
+	go spendy()
+	time.Sleep(3000 * time.Millisecond)
+	print(money)
+}
+
+// this program should give wrong output (not 100), but idk why it's not
+// race condition should occur, but it's working fine.
